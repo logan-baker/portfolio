a0155b3816f16e870e59386b678d65d573984f5f
Logan Baker <lobaker13@gmail.com>
2017-09-01 00:27:17 -0400

    links

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ $(document).ready(function() {
 
     // Let's add some nicer scrolling to the anchor tag of the portfolio section
    $('.container').localScroll({duration:1000});
+  //  $('.home-hero.parallax--bg').localScroll({duration:1000});
 });
