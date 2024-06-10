e657f94ec956bfbd662969e0823087eb1bee40dc
Logan cc-mac <lobaker13@gmail.com>
2024-06-10 11:28:56 -0400

    Update Commit message

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+    // Let's add some nicer scrolling to the anchor tag of the portfolio section
+   $('.container').localScroll({duration:1000});
+  //  $('.home-hero.parallax--bg').localScroll({duration:1000});
+});
